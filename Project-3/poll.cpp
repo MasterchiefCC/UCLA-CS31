be13c1ba4ebc78de3d2cfc25003d2ccb5aa3cf83
MasterchiefCC <chenchenwork@hotmail.com>
2016-10-24 06:14:29 +0900

    Create poll.cpp

---
+++ Project-3/poll.cpp
@@ -0,0 +1,93 @@
+#include <iostream>
+#include <string>
+#include<cctype>
+#include <cassert>
+using namespace std;
+
+bool isValidUppercaseStateCode(string stateCode)
+{
+	const string codes =
+		"AL.AK.AZ.AR.CA.CO.CT.DE.DC.FL.GA.HI.ID.IL.IN.IA.KS."
+		"KY.LA.ME.MD.MA.MI.MN.MS.MO.MT.NE.NV.NH.NJ.NM.NY.NC."
+		"ND.OH.OK.OR.PA.RI.SC.SD.TN.TX.UT.VT.VA.WA.WV.WI.WY";
+	return (stateCode.size() == 2 &&
+		stateCode.find('.') == string::npos  &&  // no '.' in stateCode
+		codes.find(stateCode) != string::npos);  // match found
+}
+
+bool hasCorrectSyntax(string pollData) {
+	for (int a = 0; a < pollData.size(); a++) {
+		if (pollData[a] == ' ') return 0;
+	}
+
+	for (int a = 0; a < pollData.size(); a++) {
+		if (isdigit(pollData[a])) {
+			if (isdigit(pollData[a + 1]))++a;
+			if (a + 1 >= pollData.size())return 0;
+			if (!isalpha(pollData[a + 1]))return 0;
+			if ((a + 1) == (pollData.size() - 1))break;
+			for (int k = 1; k <= 3; k++)
+				if (!isalpha(pollData[a + k]))return 0;
+		}
+	}
+
+	for (int a = 0; a < pollData.size(); a++) {
+		if (isalpha(pollData[a]) && a + 1 < pollData.size()) {
+			if (isalpha(pollData[a + 1]) && isdigit(pollData[a + 2])){
+				string state;
+				state += toupper(pollData[a]);
+				state += toupper(pollData[a + 1]);
+				if (!isValidUppercaseStateCode(state)) return 0;
+			}
+		}
+		return 1;
+	}
+}
+
+int countVotes(string pollData, char party, int& voteCount) {
+	if (!hasCorrectSyntax(pollData))return 1;
+
+	for (int a = 0; a < pollData.size(); a++) {
+		if (pollData[a] == '0') {
+			if (pollData[a + 1] == '0' || isalpha(pollData[a + 1]))
+				return 2;
+		}
+	}
+
+	if (!isalpha(party))return 3;
+
+	int result = 0;
+
+	for (int a = 0; a < pollData.size(); a++) {
+		if (isdigit(pollData[a])) {
+			if (isdigit(pollData[a + 1])) {
+				++a;
+				if (tolower(party) == tolower(pollData[a + 1]))
+					result += (pollData[a - 1] - '0') * 10 + (pollData[a] - '0');
+			}
+			else {
+				if (tolower(party) == tolower(pollData[a + 1]))
+					result += pollData[a] - '0';
+			}
+		}
+	}
+	voteCount = result;
+	return 0;
+}
+
+
+
+
+int main(){
+	assert(hasCorrectSyntax("TX38RCA55D"));
+	assert(!hasCorrectSyntax("MX38RCA55D"));
+	assert(!hasCorrectSyntax("MX38CA55D")); 
+	assert(!hasCorrectSyntax("MX38")); 
+	assert(!hasCorrectSyntax("MX 38"));
+	int votes;
+	votes = -999;    // so we can detect whether countVotes sets votes
+	assert(countVotes("TX38RCA55DMs6rnY29dUT06L", 'd', votes) == 0 && votes == 84);
+	votes = -999;    // so we can detect whether countVotes sets votes
+	assert(countVotes("TX38RCA55D", '%', votes) == 3 && votes == -999);
+	cout << "All tests succeeded" << endl;
+}
