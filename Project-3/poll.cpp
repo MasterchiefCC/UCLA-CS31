c1755528a94e58e663bf3558e7c8d674a905addb
MasterchiefCC <chenchenwork@hotmail.com>
2016-10-28 14:46:25 +0900

    Update poll.cpp

--- Project-3/poll.cpp
+++ Project-3/poll.cpp
@@ -23,18 +23,19 @@ bool hasCorrectSyntax(string pollData) {
 
 	for (int a = 0; a < pollData.size(); a++) {
 		if (isdigit(pollData[a])) {
-			if (isdigit(pollData[a + 1]))++a;//move to the end of digit
+			if (a+1<pollData.size()&&isdigit(pollData[a + 1]))++a;//move to the end of digit
 			if (a + 1 >= pollData.size())return 0;//end of the string
-			if (!isalpha(pollData[a + 1]))return 0;//next position is a letter
+			if (toupper(pollData[a+1]) != 'D'&&toupper(pollData[a+1]) != 'R'&&toupper(pollData[a+1]) != 'L'&&toupper(pollData[a+1]) != 'G')return 0;//next position is a letter
 			if ((a + 1) == (pollData.size() - 1))break;
 			for (int k = 1; k <= 3; k++) {//if the position is not in the end of string, next three position should be letter 
 				if (!(a + k < pollData.size()) || !isalpha(pollData[a + k]))return 0;
-				if (k == 3 && !isdigit(pollData[a + k + 1]))return 0;
+				if(k==3)
+					if ((a + k + 1 )>= pollData.size() || !isdigit(pollData[a + k + 1]))return 0;
 			}
 		}
 	}
 
-	for (int a = 0; a < pollData.size(); a++) {
+	for (int a = 0; a < pollData.size(); a++) {//Judge the char whether is a valid state code
 		if (isalpha(pollData[a]) && a + 1 < pollData.size()) {
 			if (isalpha(pollData[a + 1]) && isdigit(pollData[a + 2])){
 				string state;
@@ -43,25 +44,26 @@ bool hasCorrectSyntax(string pollData) {
 				if (!isValidUppercaseStateCode(state)) return 0;
 			}
 		}
-		return 1;
 	}
+	
+	return 1;
 }
 
 int countVotes(string pollData, char party, int& voteCount) {
-	if (!hasCorrectSyntax(pollData))return 1;
+	if (!hasCorrectSyntax(pollData))return 1;//if can't pass the function 1, return 1
 
-	for (int a = 0; a < pollData.size(); a++) {
+	for (int a = 0; a < pollData.size(); a++) {//judge whether contain 0 or 00
 		if (pollData[a] == '0') {
 			if (pollData[a + 1] == '0' || isalpha(pollData[a + 1]))
 				return 2;
 		}
 	}
 
-	if (!isalpha(party))return 3;
+	if (!isalpha(party))return 3;//if the party input is not a valid char return 3
 
 	int result = 0;
 
-	for (int a = 0; a < pollData.size(); a++) {
+	for (int a = 0; a < pollData.size(); a++) {//add the result
 		if (isdigit(pollData[a])) {
 			if (isdigit(pollData[a + 1])) {
 				++a;
@@ -79,7 +81,7 @@ int countVotes(string pollData, char party, int& voteCount) {
 }
 
 int main(){
-	assert(!hasCorrectSyntax("CA00"));
+	assert(!hasCorrectSyntax("CA55DAL"));
 	assert(!hasCorrectSyntax("MX38RCA55D"));
 	assert(!hasCorrectSyntax("MX38CA55D")); 
 	assert(!hasCorrectSyntax("MX38")); 
